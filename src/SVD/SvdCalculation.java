354f36777606a86ee3336ea593b54b962a7e89b7
Nadim1028 <bsse1028@iit.du.ac.bd>
2022-07-18 01:02:07 +0600

    Jama matrix works for only when rows are greater than or equals columns

--- src/SVD/SvdCalculation.java
+++ src/SVD/SvdCalculation.java
@@ -21,25 +21,33 @@ public class SvdCalculation {
 //         000000011
 
         // create M-by-N matrix that doesn't have full rank
-        int  row = 5,col = 1;
-        Matrix A  = new Matrix(new double[][]
+        int  numberOfDigitAfterDecimalPoint = 2,col = 3;//row is the number of digit after decimal point
+      /*  Matrix A  = new Matrix(new double[][]
                 {{3,3,2}, {2,3,-2},{0,0,0}}
-                );
+                );*/
 
-//        Matrix A = new Matrix(new double[][] {
-//                {1, 0, 0, 1, 0, 0, 0, 0, 0 },
-//                {1, 0, 1, 0, 0, 0, 0, 0, 0 },
-//                {1, 1, 0, 0, 0, 0, 0, 0, 0 },
-//                {0, 1, 1, 0, 1, 0, 0, 0, 0 },
-//                {0, 1, 1, 2, 0, 0, 0, 0, 0 },
-//                {0, 1, 0, 0, 1, 0, 0, 0, 0 },
-//                {0, 1, 0, 0, 1, 0, 0, 0, 0 },
-//                {0, 0, 1, 1, 0, 0, 0, 0, 0 },
-//                {0, 1, 0, 0, 0, 0, 0, 0, 1 },
-//                {0, 0, 0, 0, 0, 1, 1, 1, 0 },
-//                {0, 0, 0, 0, 0, 0, 1, 1, 1 },
-//                {0, 0, 0, 0, 0, 0, 0, 1, 1 }
-//        });
+       /* Matrix A  = new Matrix(new double[][]
+                {{2,3}, {4,10}}
+        );*/
+
+        Matrix A  = new Matrix(new double[][]
+                {{1,1,1}, {-1,-3,-3},{2,4,4}}
+        );
+
+       /* Matrix A = new Matrix(new double[][] {
+                {1, 0, 0, 1, 0, 0, 0, 0, 0 },
+                {1, 0, 1, 0, 0, 0, 0, 0, 0 },
+                {1, 1, 0, 0, 0, 0, 0, 0, 0 },
+                {0, 1, 1, 0, 1, 0, 0, 0, 0 },
+                {0, 1, 1, 2, 0, 0, 0, 0, 0 },
+                {0, 1, 0, 0, 1, 0, 0, 0, 0 },
+                {0, 1, 0, 0, 1, 0, 0, 0, 0 },
+                {0, 0, 1, 1, 0, 0, 0, 0, 0 },
+                {0, 1, 0, 0, 0, 0, 0, 0, 1 },
+                {0, 0, 0, 0, 0, 1, 1, 1, 0 },
+                {0, 0, 0, 0, 0, 0, 1, 1, 1 },
+                {0, 0, 0, 0, 0, 0, 0, 1, 1 }
+        });*/
 
        /* Matrix B = Matrix.random(5, 3);
         Matrix A = Matrix.random(col, row).times(B).times(B.transpose());*/
@@ -48,21 +56,25 @@ public class SvdCalculation {
         System.out.println("Col = "+A.getColumnDimension());
 
         System.out.print("A = "  );
-        A.print(col,row);
+        A.print(col,numberOfDigitAfterDecimalPoint);
 
         // compute the singular vallue decomposition
         System.out.println("A = U S V^T");
         System.out.println();
         SingularValueDecomposition s = A.svd();
+
         System.out.print("U = ");
         Matrix U = s.getU();
-        U.print(col, 3);
+        U.print(col, numberOfDigitAfterDecimalPoint);
+
         System.out.print("Sigma = ");
         Matrix S = s.getS();
-        S.print(col, row);
+        S.print(col, numberOfDigitAfterDecimalPoint);
+
         System.out.print("V = ");
         Matrix V = s.getV();
-        V.print(col, row);
+        V.print(col, numberOfDigitAfterDecimalPoint);
+
         System.out.println("rank = " + s.rank());
         System.out.println("condition number = " + s.cond());
         System.out.println("2-norm = " + s.norm2());
@@ -70,7 +82,7 @@ public class SvdCalculation {
         // print out singular values
         System.out.print("singular values = ");
         Matrix svalues = new Matrix(s.getSingularValues(), 1);
-        svalues.print(col, row);
+        svalues.print(col, numberOfDigitAfterDecimalPoint);
     }
 
 }
