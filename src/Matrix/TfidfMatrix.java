cbb381a50fde2e56c597686af917999041d3d98d
Nadim1028 <bsse1028@iit.du.ac.bd>
2022-07-18 20:06:33 +0600

    Term Frequency Matrix built

---
+++ src/Matrix/TfidfMatrix.java
@@ -0,0 +1,7 @@
+package Matrix;
+
+public class TfidfMatrix {
+        int totalWordsInEachDoc,numOfDoc,wordFrequencyInEachDoc,wordFrequencyInAllDocs;
+
+
+}
--- src/TestSimilarity.java
+++ src/TestSimilarity.java
@@ -1,25 +1,78 @@
 import CosineSimilarity.CosineAngleCalculator;
+import Jama.Matrix;
+import Matrix.TokenFinderInDocs;
 import edu.ucla.sspace.vector.DoubleVector;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Map;
-import java.util.Set;
-import java.util.Vector;
+import java.util.*;
 
 public class TestSimilarity
 {
     public static void main(String[] args) throws IOException
     {
-        String filePath1="src/Output/clean_data1.txt",filePath2="src/Output/clean_data2.txt";
+        String filePath1="src/Output/clean_data1.txt",filePath2="src/Output/clean_data2.txt",filePath3="src/Output/nadim.txt";
         int[]  valueOfTerms1,valueOfTerms2;
         LatentSemanticAnalysis latentSemanticAnalysis = new LatentSemanticAnalysis();
         Map<String,Integer> termCounts1=latentSemanticAnalysis.getTermsCounts(filePath1);
         Map<String,Integer> termCounts2=latentSemanticAnalysis.getTermsCounts(filePath2);
-        Map<String,Integer> termCounts3=latentSemanticAnalysis.getTermsCounts(filePath2);
-        Map<String,Integer> termCounts4=latentSemanticAnalysis.getTermsCounts(filePath1);
+        Map<String,Integer> termCounts3=latentSemanticAnalysis.getTermsCounts(filePath3);
 
-        /*Map<String,Integer> termCounts1=latentSemanticAnalysis.getTermsCounts(filePath1);
+
+        System.out.println("doc1 unique keyword size : "+termCounts1.size());
+        System.out.println("doc2 unique keyword size : "+termCounts2.size());
+        System.out.println("doc3 unique keyword size : "+termCounts3.size());
+
+        System.out.println("*****Words frequency in each docs.*****");
+
+        System.out.println("Doc1 : "+termCounts1);
+        System.out.println("Doc2 : "+termCounts2);
+        System.out.println("Doc3 : "+termCounts3);
+
+
+        Set<String> str = latentSemanticAnalysis.getWords();
+        System.out.println("*******************TokensInAllDocuments****************************");
+        System.out.println(str);
+        System.out.println("total tokens size : " + str.size());
+
+        System.out.println("*****CastInsensitiveUniqueTokens*****");
+        Set<String> caseInsensitiveTokenSet= new HashSet<>();
+        for(String token : str){
+            String lowerCaseString = token.toLowerCase();
+            //System.out.println(lowerCaseString);
+            caseInsensitiveTokenSet.add(lowerCaseString);
+        }
+
+
+        List<String> sortedList = new ArrayList<>(caseInsensitiveTokenSet);
+        Collections.sort(sortedList);
+        System.out.println("Sorted :"+sortedList);
+        System.out.println("CastInsensitiveUniqueTokensSize : "+caseInsensitiveTokenSet.size());
+
+        double[][] matrix = new double[sortedList.size()][3];
+
+        for(int i=0;i<sortedList.size();i++){
+            int wordFrequency1 = new TokenFinderInDocs(termCounts1,sortedList.get(i)).getTermFrequency();
+            int wordFrequency2 = new TokenFinderInDocs(termCounts2,sortedList.get(i)).getTermFrequency();
+            int wordFrequency3 = new TokenFinderInDocs(termCounts3,sortedList.get(i)).getTermFrequency();
+            //System.out.println(sortedList.get(i) + ":" + "d1="+wordFrequency1+",d2="+wordFrequency2 + ",d3="+wordFrequency3);
+            matrix[i][0] = wordFrequency1;
+            matrix[i][1] = wordFrequency2;
+            matrix[i][2] = wordFrequency3;
+        }
+        System.out.println("d1   d2   d3");
+        for (int i = 0; i < matrix.length; i++) {
+            // Loop through all elements of current row
+
+            for (int j = 0; j < matrix[i].length; j++)
+                System.out.print(matrix[i][j] + "   ");
+            System.out.println("<=== "+sortedList.get(i));
+        }
+
+    }
+
+
+     /*Map<String,Integer> termCounts1=latentSemanticAnalysis.getTermsCounts(filePath1);
         Map<String,Integer> termCounts2=latentSemanticAnalysis.getTermsCounts(filePath2);
 
         valueOfTerms1 = new int[termCounts1.size()];
@@ -40,18 +93,6 @@ public class TestSimilarity
         printTermsValue(termCounts1,termCounts2);
         System.out.println("Cosine Similarity = "+(result*100)+"%");*/
 
-
-        Set<String> str = latentSemanticAnalysis.getWords();
-        System.out.println("*******************Tokens****************************");
-        System.out.println(str);
-        System.out.println("*****************************************************");
-
-        latentSemanticAnalysis.processSpace(System.getProperties());
-        System.out.println("Document Space ======= "+latentSemanticAnalysis.documentSpace.rows());
-        System.out.println("Vector  ============== "+latentSemanticAnalysis.getDocumentVector(3));
-
-
-    }
 
     public static void printTermsValue( Map<String,Integer> termCounts1, Map<String,Integer> termCounts2){
         System.out.println("*************FirstDocument**************");
